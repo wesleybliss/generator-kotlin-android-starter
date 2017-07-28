233763462ceadf7c489a3f555e698ff4eb3a0961
Wesley Bliss <wesley.bliss@gmail.com>
2017-07-28 03:41:18 -0400

    Updated main entrypoint & stubbed out helper script to clone & sync Android project repo

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,7 @@
 'use strict'
 const fs = require('fs')
 const path = require('path')
+const mkdirp = require('mkdirp')
 const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
@@ -34,6 +35,18 @@ module.exports = class extends Generator {
     writing() {
         
         this.log(JSON.stringify(this.props, null, '    '))
+        
+        const namespace = this.props.appNamespace.replace(/\./g, '/')
+        
+        this.log(JSON.stringify({
+            namespace,
+            sourceRoot: this.sourceRoot()
+        }, null, '    '))
+        
+        mkdirp('app/src')
+        mkdirp('app/src/test')
+        mkdirp('app/src/androidTest')
+        mkdirp(`app/src/main/kotlin/${namespace}`)
         
         /*this.fs.copy(
             this.templatePath('dummyfile.txt'),
